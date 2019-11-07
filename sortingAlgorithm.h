ceea162a48cbb726306a5aa3f1b78590c000e8ca
ChristopherBlackman <christopherblackman@localhost.localdomain>
2019-11-07 15:34:21 -0500

    added a guard, cleaned repo

--- sortingAlgorithm.h
+++ sortingAlgorithm.h
@@ -1,3 +1,6 @@
+#ifndef SORTING_ALGORITHM_H
+#define SORTING ALGORITHM_H
 void bubbleSort(int* arr,int count);
 void bubbleSort1();
 void bubbleSort2(void* arr, int count,int size,const void* compare);
+#endif
